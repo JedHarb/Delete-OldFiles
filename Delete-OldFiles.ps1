e2c9e9bd3314bbd55019deb94ccfe0ed15fab50e
Jared Harber <35547112+JedHarb@users.noreply.github.com>
2024-01-20 21:41:24 -0600

    Update Delete-OldFiles.ps1

--- Delete-OldFiles.ps1
+++ Delete-OldFiles.ps1
@@ -1,10 +1,12 @@
 # Put this script into the folder you want to clean up before you run it. Or optionally, define a -Path in each Get-ChildItem command.
-"Checking files in this folder: $((Get-Location).ProviderPath)`nPlease wait...`n"
+"Warning: this script is designed to help you clean up user-created folders. It will permanently delete old files, including hidden files. Do not run this in system directories!"
+Read-Host "Ready to check files in this folder: $((Get-Location).ProviderPath)`nPress ENTER to proceed. (Or close this script to exit.)"
+"Please wait...`n"
 
-# Get all files older than 30 days in the current directory. Feel free to adjust this number.
+# Get all files older than 30 days (feel free to adjust this number) in the current folder, plus subfolders, including hidden files.
 $FilesToDelete = (Get-ChildItem -Recurse -Force -File).Where({$_.LastWriteTime -lt (Get-Date).AddDays(-30)})
 
-Read-Host "CAUTION: Press ENTER to permanently delete $($FilesToDelete.count) files that are older than 1 month, and then empty folders, from this folder $((Get-Location).ProviderPath) !!! If you do NOT wish to do this, close the script now!"
+Read-Host "CAUTION: Press ENTER to PERMANENTLY DELETE $($FilesToDelete.count) files that are older than 30 days, and then empty folders, from this folder $((Get-Location).ProviderPath)!!!`nIf you do NOT wish to do this, close the script now!"
 $FilesToDelete | Remove-Item -Confirm:$false # Delete files before folders
 
 # Get all empty folders (including folders that contain only empty folders recursively) in the current directory and delete them. Feel free to remove this if you only care about removing the files above.
